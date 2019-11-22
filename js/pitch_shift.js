f948fd41b51047a8fbca914c9256ce3d7a6672cf
我如古 正志 <xyy03515@ad.in.infocom.co.jp>
2019-11-22 18:26:08 +0900

    use class

--- js/pitch_shift.js
+++ js/pitch_shift.js
@@ -1,148 +1,167 @@
-    // --- voice pitch shifter ---
-    let voiceChangeStream = null;
-    let audioContext = null;
-    let audioSrcNode = null;
-    let mediaDestNode = null;
-    let pitchShifterProcessor = null;
-    let normalGainNode = null;
-    let shiftGainNode = null;
-    let pitchRatio = 0.7;
-    let overlapRatio = 0.5;
-    const grainSize = 512;
+'use static'
 
-    function setPitch(v) {
-      pitchRatio = v;
-      normalGainNode.gain.value = 0;
-      shiftGainNode.gain.value = 1.0;
+// --- voice pitch shifter ---
+
+class PitchShifter {
+  // NO static property// --- static property ---
+  //static audioContext = null;
+  //static grainSize = 512;
+
+  // --- static function ---
+  static _stopMeidaStream(stream, label) {
+    if (stream) {
+      stream.getTracks().forEach(track => {
+        console.log('stop ' + label + ' track:', track);
+        track.stop();
+      });
+    }
+  }
+
+
+  // --- constructor ----
+  constructor(pitch) {
+    // --- instance property ---
+    this.pitchRatio = pitch;
+
+    this.voiceChangeStream = null;
+    this.audioSrcNode = null;
+    this.mediaDestNode = null;
+    this.pitchShifterProcessor = null;
+    this.normalGainNode = null;
+    this.shiftGainNode = null;
+    this.overlapRatio = 0.5;
+    this.grainSize = 512;
+  }
+
+  // --- instance function ---
+  setPitch(v) {
+    this.pitchRatio = v;
+    this.pitchShifterProcessor.pitchRatio = this.pitchRatio;
+    this.normalGainNode.gain.value = 0;
+    this.shiftGainNode.gain.value = 1.0;
+  }
+
+  setNormalPitch() {
+    this.pitchRatio = 1.0;
+    this.pitchShifterProcessor.pitchRatio = this.pitchRatio;
+    this.normalGainNode.gain.value = 1.0;
+    this.shiftGainNode.gain.value = 0;
+  }
+
+  setMute() {
+    this.normalGainNode.gain.value = 0;
+    this.shiftGainNode.gain.value = 0;
+  }
+
+  clearAudioNode() {
+    PitchShifter._stopMeidaStream(this.voiceChangeStream, 'voiceChangeStream');
+    this.voiceChangeStream = null;
+
+    if (this.mediaDestNode) {
+      this.mediaDestNode.disconnect();
+      this.mediaDestNode = null;
     }
 
-    function setNormalPitch() {
-      pitchRatio = 1.0;
-      normalGainNode.gain.value = 1.0;
-      shiftGainNode.gain.value = 0;
+    if (this.normalGainNode) {
+      this.normalGainNode.disconnect();
+      this.normalGainNode = null;
+    }
+    if (this.shiftGainNode) {
+      this.shiftGainNode.disconnect();
+      this.shiftGainNode = null;
     }
 
-    function setMute() {
-      normalGainNode.gain.value = 0;
-      shiftGainNode.gain.value = 0;
+    if (this.pitchShifterProcessor) {
+      this.pitchShifterProcessor.disconnect();
+      this.pitchShifterProcessor = null;
     }
 
-    function clearAudioNode() {
-      _stopMeidaStream(voiceChangeStream, 'voiceChangeStream');
-      voiceChangeStream = null;
-
-      if (mediaDestNode) {
-        mediaDestNode.disconnect();
-        mediaDestNode = null;
-      }
-
-      if (normalGainNode) {
-        normalGainNode.disconnect();
-        normalGainNode = null;
-      }
-      if (shiftGainNode) {
-        shiftGainNode.disconnect();
-        shiftGainNode = null;
-      }
-
-      if (pitchShifterProcessor) {
-        pitchShifterProcessor.disconnect();
-        pitchShifterProcessor = null;
-      }
-
-      if (audioSrcNode) {
-        audioSrcNode.disconnect();
-        audioSrcNode = null;
-      }
+    if (this.audioSrcNode) {
+      this.audioSrcNode.disconnect();
+      this.audioSrcNode = null;
     }
+  }
 
-    function initSoundProcesser(stream) {
-      if (!audioContext) {
-        audioContext = new window.AudioContext();
+  initSoundProcesser(audioContext, stream) {
+    this.clearAudioNode();
+
+    this.audioSrcNode = audioContext.createMediaStreamSource(stream);
+    this.mediaDestNode = audioContext.createMediaStreamDestination();
+    this.voiceChangeStream = this.mediaDestNode.stream;
+
+    this.normalGainNode = audioContext.createGain();
+    this.normalGainNode.gain.value = 0;
+    this.audioSrcNode.connect(this.normalGainNode);
+    // this.normalGainNode.connect(audioContext.destination);
+    this.normalGainNode.connect(this.mediaDestNode);
+
+    // ---- pitch shifter ---
+    this.pitchShifterProcessor = audioContext.createScriptProcessor(this.grainSize, 1, 1);
+    this.pitchShifterProcessor.buffer = new Float32Array(this.grainSize * 2);
+    this.pitchShifterProcessor.grainWindow = hannWindow(this.grainSize);
+    this.pitchShifterProcessor.grainSize = this.grainSize;
+    this.pitchShifterProcessor.pitchRatio = this.pitchRatio;
+    this.pitchShifterProcessor.overlapRatio = this.overlapRatio;
+    this.pitchShifterProcessor.onaudioprocess = function (event) {
+      // this === pitchShiftProcessor
+
+      const inputData = event.inputBuffer.getChannelData(0);
+      const outputData = event.outputBuffer.getChannelData(0);
+
+      for (let i = 0; i < inputData.length; i++) {
+        // Apply the window to the input buffer
+        inputData[i] *= this.grainWindow[i];
+
+        // Shift half of the buffer
+        this.buffer[i] = this.buffer[i + this.grainSize];
+
+        // Empty the buffer tail
+        this.buffer[i + this.grainSize] = 0.0;
       }
-      clearAudioNode();
 
-      audioSrcNode = audioContext.createMediaStreamSource(stream);
-      mediaDestNode = audioContext.createMediaStreamDestination();
-      voiceChangeStream = mediaDestNode.stream;
+      // Calculate the pitch shifted grain re-sampling and looping the input
+      const grainData = new Float32Array(this.grainSize * 2);
+      for (let i = 0, j = 0.0; i < this.grainSize; i++ , j += this.pitchRatio) {
+        const index = Math.floor(j) % this.grainSize;
+        const a = inputData[index];
+        const b = inputData[(index + 1) % this.grainSize];
+        grainData[i] += linearInterpolation(a, b, j % 1.0) * this.grainWindow[i];
+      }
 
-      normalGainNode = audioContext.createGain();
-      normalGainNode.gain.value = 0;
-      audioSrcNode.connect(normalGainNode);
-      //normalGainNode.connect(audioContext.destination);
-      normalGainNode.connect(mediaDestNode);
-
-      // ---- pitch shifter ---
-      pitchShifterProcessor = audioContext.createScriptProcessor(grainSize, 1, 1);
-      pitchShifterProcessor.buffer = new Float32Array(grainSize * 2);
-      pitchShifterProcessor.grainWindow = hannWindow(grainSize);
-      pitchShifterProcessor.onaudioprocess = function (event) {
-        const inputData = event.inputBuffer.getChannelData(0);
-        const outputData = event.outputBuffer.getChannelData(0);
-
-        for (let i = 0; i < inputData.length; i++) {
-          // Apply the window to the input buffer
-          inputData[i] *= this.grainWindow[i];
-
-          // Shift half of the buffer
-          this.buffer[i] = this.buffer[i + grainSize];
-
-          // Empty the buffer tail
-          this.buffer[i + grainSize] = 0.0;
+      // Copy the grain multiple times overlapping it
+      for (let i = 0; i < this.grainSize; i += Math.round(this.grainSize * (1.0 - this.overlapRatio))) {
+        for (let j = 0; j <= this.grainSize; j++) {
+          this.buffer[i + j] += grainData[j];
         }
-
-        // Calculate the pitch shifted grain re-sampling and looping the input
-        const grainData = new Float32Array(grainSize * 2);
-        for (let i = 0, j = 0.0; i < grainSize; i++ , j += pitchRatio) {
-          const index = Math.floor(j) % grainSize;
-          const a = inputData[index];
-          const b = inputData[(index + 1) % grainSize];
-          grainData[i] += linearInterpolation(a, b, j % 1.0) * this.grainWindow[i];
-        }
-
-        // Copy the grain multiple times overlapping it
-        for (let i = 0; i < grainSize; i += Math.round(grainSize * (1.0 - overlapRatio))) {
-          for (let j = 0; j <= grainSize; j++) {
-            this.buffer[i + j] += grainData[j];
-          }
-        }
-
-        // Output the first half of the buffer
-        for (let i = 0; i < grainSize; i++) {
-          outputData[i] = this.buffer[i];
-        }
-      };
-      audioSrcNode.connect(pitchShifterProcessor);
-
-      shiftGainNode = audioContext.createGain();
-      shiftGainNode.gain.value = 1.0;
-      pitchShifterProcessor.connect(shiftGainNode);
-      //shiftGainNode.connect(audioContext.destination);
-      shiftGainNode.connect(mediaDestNode);
-
-      //modifiedStream.addTrack(voiceChangeStream.getAudioTracks()[0]);
-      return voiceChangeStream;
-
-      //---------------- inner function ----------------
-      function hannWindow(length) {
-        const hwindow = new Float32Array(length);
-        for (var i = 0; i < length; i++) {
-          hwindow[i] = 0.5 * (1 - Math.cos(2 * Math.PI * i / (length - 1)));
-        }
-        return hwindow;
-      };
-
-      function linearInterpolation(a, b, t) {
-        return a + (b - a) * t;
-      };
-    }
-
-    function _stopMeidaStream(stream, label) {
-      if (stream) {
-        stream.getTracks().forEach(track => {
-          console.log('stop ' + label + ' track:', track);
-          track.stop();
-        });
       }
-    }
-    // --- voice pitch shifter ---
+
+      // Output the first half of the buffer
+      for (let i = 0; i < this.grainSize; i++) {
+        outputData[i] = this.buffer[i];
+      }
+    };
+    this.audioSrcNode.connect(this.pitchShifterProcessor);
+
+    this.shiftGainNode = audioContext.createGain();
+    this.shiftGainNode.gain.value = 1.0;
+    this.pitchShifterProcessor.connect(this.shiftGainNode);
+    //this.shiftGainNode.connect(audioContext.destination); // test
+    this.shiftGainNode.connect(this.mediaDestNode);
+
+    return this.voiceChangeStream;
+
+    //---------------- inner function ----------------
+    function hannWindow(length) {
+      const hwindow = new Float32Array(length);
+      for (var i = 0; i < length; i++) {
+        hwindow[i] = 0.5 * (1 - Math.cos(2 * Math.PI * i / (length - 1)));
+      }
+      return hwindow;
+    };
+
+    function linearInterpolation(a, b, t) {
+      return a + (b - a) * t;
+    };
+  }
+}
+// --- voice pitch shifter ---
